7846b13cc2776a8eaf23790487f06f5dc085865e
Jingwen Cao <racheljcao@gmail.com>
2019-03-20 21:35:13 -0400

    changes

---
+++ breastcancer_analysis.py
@@ -0,0 +1,24 @@
+# 1. Import all libraries
+import numpy as np
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as ppt
+from argparse import ArgumentParser
+from sklearn import neighbors
+from sklearn import metrics
+from sklearn.datasets import load_breast_cancer
+
+# 2. Split data into training data, test data, and validation data
+
+# 3. Perform all classifications using various models
+# 3a. Perform logistic regression
+# 3b. Perform support vector machines
+# 3c. Perform stochastic gradient descent
+# 3d. Perform nearest neighbor
+# 3e. Perform naive bayes
+# 3f. Perform decision trees
+# 3g. Perform ensemble methods
+
+#4. Check accuracy for various models
+
+#5. Plot model accuracy
